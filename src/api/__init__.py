b58331ed704376c230c75fa36dcfcb2f9880d059
Sumiyyah-Ahmed <sumiyyah@gmail.com>
2021-10-02 14:38:59 -0500

    relative path fix

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -3,12 +3,13 @@ import sqlite3
 from flask import Flask, send_from_directory
 from flask_api import status as s
 
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-react_path = os.path.join(BASE_DIR, "frontend", "build")
-db_path = os.path.join(BASE_DIR, "database", "sms.db")
+source_path = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(source_path)
+react_path = os.path.join(source_path, "frontend", "build")
+db_path = os.path.join(source_path, "database", "sms.db")
 
 app = Flask(__name__, static_url_path='', static_folder=react_path)
-db = sqlite3.connect(db_path)
+db = sqlite3.connect(os.path.relpath(db_path, BASE_DIR))
 
 @app.route("/", defaults={'path':''})
 def serve(path):
