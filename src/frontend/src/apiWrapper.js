6ebbbceb37fd6c8113ad1c8336ee6fa6ec8e1dab
Sumiyyah-Ahmed <sumiyyah@gmail.com>
2021-10-02 22:31:53 -0500

    'sure'

--- src/frontend/src/apiWrapper.js
+++ src/frontend/src/apiWrapper.js
@@ -1,5 +1,3 @@
-import { useCallback } from "react";
-
 require("dotenv").config();
 
 const host = process.env.HOST || 'localhost';
@@ -8,3 +6,4 @@ const port = process.env.PORT || 5000;
 export const baseUrl = `http://${host}:${port}`;
 export const apiEndpoint = `${baseUrl}/api`;
 export const incidentReportsEndpoint = `${apiEndpoint}/incident`;
+export const allIncidentsEndpoint = `${incidentReportsEndpoint}/all`;
